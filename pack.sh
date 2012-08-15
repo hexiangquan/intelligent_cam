a9fcf9198c8441918298167c2e599aa7894398a0
sun <shengkai.sun@gmail.com>
2012-08-15 17:50:24 +0800

    add echo for packing

--- pack.sh
+++ pack.sh
@@ -4,18 +4,24 @@
 # History:
 # 2012/8/15		Sun 	Created
 
-cd ..
 
-DIR="HDCAM"
-DATE=`date +%Y.%m.%d`
 PRJ="hdcam"
 DST="/home/sun/work/ext-disc/Projects/HDCAM/ARM/Application"
+DIR="HDCAM"
+
+DATE=`date +%Y.%m.%d`
 TAR_FILE="$PRJ.$DATE.tar.gz"
 
+cd ..
+
+echo -e "tar dir $DIR to $TAR_FILE..."
 tar zcf $TAR_FILE $DIR
 
-if [ $? == 0  ] && [ -d "$DST" ]; then
+if [ -d "$DST" ]; then
+	echo -e "copy $TAR_FILE to $DST..."
 	cp -p $TAR_FILE $DST 
 fi
+
+echo -e "done.\n"
 
 exit 0
